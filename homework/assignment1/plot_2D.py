1dfd8d2e0748300710b50bc5d7eed0acfd2800c6
lpyrubber <oliverlai134@gmail.com>
2024-09-18 16:39:44 -0400

    change figu

--- homework/assignment1/plot_2D.py
+++ homework/assignment1/plot_2D.py
@@ -5,13 +5,15 @@ data = np.loadtxt("data.txt")
 #print(data)
 x = np.array(data[0,:]).reshape((51,51))
 y = np.array(data[1,:]).reshape((51,51))
+y = y.transpose()
+print(y)
 
 plt.figure()
 for i in range(51):
     plt.plot(x[:,i],y[:,i],'-k',linewidth=0.5)
     plt.plot(x[i,:],y[i,:],'-k',linewidth=0.5)
 
-plt.xlim([-1,2])
-plt.ylim([0,1.5])
+#plt.xlim([-1,2])
+#plt.ylim([0,1.5])
 plt.title("Mesh near airfoil")
 plt.savefig('grid.png')
